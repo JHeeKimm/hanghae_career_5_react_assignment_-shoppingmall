bb55b05d4be58a7e43946c53b9b4e1a43d5f6905
JheeKim <jheekim992@gmail.com>
2024-10-11 10:25:26 +0900

    Fix: 캐시 무효화로 상품 등록 후, 리스트 최신화

--- src/lib/hooks/useAddProduct.ts
+++ src/lib/hooks/useAddProduct.ts
@@ -1,4 +1,4 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { ProductSliceState } from '@/types/productType';
 import { IProduct, NewProductDTO } from '@/api/dtos/productDTO';
 import { addProductAPI } from '@/api/product';
@@ -6,6 +6,7 @@ import { useProductStore } from '@/store/useProductStore';
 
 export const useAddProduct = () => {
   const { setAdd, setError } = useProductStore();
+  const queryClient = useQueryClient();
 
   return useMutation<IProduct, Error, NewProductDTO>({
     mutationFn: async (userData: NewProductDTO) => {
@@ -13,6 +14,7 @@ export const useAddProduct = () => {
     },
     onSuccess: (newProduct) => {
       setAdd(newProduct);
+      queryClient.invalidateQueries({ queryKey: ['products'] });
     },
     onError: () => {
       setError();
